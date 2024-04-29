1dc47c8ffd507de5874ed165677a3bcaa6665313
vanessappereira <vanessappereira@outlook.pt>
2024-04-29 20:15:31 +0100

    Added 4th day

--- day_4/1_circular_graphic.py
+++ day_4/1_circular_graphic.py
@@ -1,4 +1,10 @@
-import matplotlib as plt;
+import matplotlib.pyplot as plt;
+
 #Gráfico circular
 
-frutas = ["Maçã", "Bananas", "Pêras", "Laranjas"]
+frutas = ['Maçãs', 'Bananas', 'Pêras', 'Laranjas'];
+quantidades_vendidas = [20, 25, 15, 40];
+
+plt.pie(quantidades_vendidas, labels=frutas, autopct='%1.1f%%');
+plt.title('Gráfico Circular');
+plt.show();
--- day_4/2_exemplo_histograma_mediana.py
+++ day_4/2_exemplo_histograma_mediana.py
@@ -1,10 +1,10 @@
-import matplotlib as plt;
+import matplotlib.pyplot as plt
 import numpy as np;
 
 dados = np.random.normal(loc=0,scale=1,size=100);
 mediana = np.median(dados);
 
-plt.hist(dados, bin=20, alpha=0.5, color='blue', edgecolor='black');
+plt.hist(dados, bins=20, alpha=0.5, color='blue', edgecolor='black');
 plt.axvline(mediana, color='red', linestyle='dashed', linewidth=1);
 
 plt.xlabel('Valores');
