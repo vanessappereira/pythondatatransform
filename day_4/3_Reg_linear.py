4661b65e1ddeae8db9537691a1106da1ca8f931f
vanessappereira <vanessappereira@outlook.pt>
2024-04-29 20:11:58 +0100

    Added 4th day

--- day_4/3_Reg_linear.py
+++ day_4/3_Reg_linear.py
@@ -1,16 +1,16 @@
-import matplotlib.pylot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
-x = [1, 2, 3, 4, 5, 6];
-y = [2, 3, 4, 5, 6, 7];
+x = [1, 2, 3, 4, 5, 6]
+y = [2, 3, 4, 5, 6, 7]
 
-m, b = np.polyfit(x, y, 1);
+m, b = np.polyfit(x, y, 1)
 
-plt.scatter(x, y, color="green", label="Dados");
-plt.plot(x, m * x + b, color='red', label='Regressão Linear');
+plt.scatter(x, y, color="green", label="Dados")
+plt.plot(x, m * (x + b), color="red", label="Regressão Linear")
 
-plt.xlabel("Eixo X");
-plt.ylabel("Eixo Y");
-plt.title("Gráfico de Regressão Linear");
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
+plt.title("Gráfico de Regressão Linear")
 
-plt.show();
+plt.show()
